4b8caafe1c6409f79d73558846df4ce2fc529a4f
Rexe <barmonrohit216@gmail.com>
2026-03-15 01:23:32 +0530

    Update version-check.sh

--- scripts/version-check.sh
+++ scripts/version-check.sh
@@ -1,11 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-VERSION="v152"
-
-LATEST=$(curl -s https://api.github.com/repos/rexecoder125/REXE-v151-ELITE/releases/latest | grep tag_name | cut -d '"' -f4)
-
-if [ "$VERSION" != "$LATEST" ]; then
-echo "New version available: $LATEST"
-else
-echo "You are using the latest version"
-fi
+source config/version.conf
+echo "REXE Version: $VERSION"
