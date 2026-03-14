7d03966817b272a8dc2a0fd7c61485b3e27c14e5
Rexe <barmonrohit216@gmail.com>
2026-03-14 19:48:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
 clear
 echo "Installing REXE v151 ELITE..."
 
 pkg update -y
-pkg install git zsh curl wget figlet toilet neofetch htop -y
+pkg install git zsh curl wget figlet toilet neofetch -y
 
-mkdir -p ~/.termux
-cp theme/colors.properties ~/.termux/colors.properties 2>/dev/null
+# Kali Linux Style Prompt
+echo 'PS1="\[\e[1;32m\]┌──(rexe㉿kali)-[\w]\n└─$ \[\e[0m\]"' >> ~/.bashrc
 
-echo "bash banner/banner.sh" >> ~/.bashrc
+# Auto banner
+echo "bash ~/REXE-v151-ELITE/banner/banner.sh" >> ~/.bashrc
 
-echo "Installation complete."
+echo "Installation complete!"
