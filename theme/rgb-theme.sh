260855c1442ee6260f9b4a6cb789bdcae3056fb4
Rexe <barmonrohit216@gmail.com>
2026-03-15 01:11:21 +0530

    Create rgb-theme.sh

---
+++ theme/rgb-theme.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+while true
+do
+echo -e "\e[1;31mREXE"
+sleep 0.1
+echo -e "\e[1;32mREXE"
+sleep 0.1
+echo -e "\e[1;33mREXE"
+sleep 0.1
+echo -e "\e[1;34mREXE"
+sleep 0.1
+echo -e "\e[1;35mREXE"
+sleep 0.1
+echo -e "\e[1;36mREXE"
+sleep 0.1
+done
